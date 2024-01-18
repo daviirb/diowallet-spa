d4520911bccefd4339c94d8f949b3c9d57172e82
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-18 19:21:56 -0300

    feat: components input and button

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/prop-types
+export default function Button({ type, text }) {
+  return (
+    <button
+      type={type}
+      className="px-4 py-2 rounded w-full font-bold text-white bg-gradient-to-r from-sky-500 to-indigo-500"
+    >
+      {text}
+    </button>
+  );
+}
