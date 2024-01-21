ac4c5688ac315d650e5f13864a08d62beb96a7f2
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-21 13:36:28 -0300

    feat: Error message

---
+++ src/components/ErrorInput.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+export default function ErrorInput({ text }) {
+  return (
+    <span className="bg-red-100 text-red-500 rounded p-2 text-sm">{text}</span>
+  );
+}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line react/prop-types
-export default function Input({ type, placeholder }) {
+export default function Input({ type, placeholder, register, name }) {
   return (
     <input
       type={type}
       placeholder={placeholder}
-      className="rounded p-2 w-full "
+      className="rounded p-2 w-full"
+      {...register(name)}
     />
   );
 }
