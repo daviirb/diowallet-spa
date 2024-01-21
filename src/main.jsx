844d3b9cede8b9e35a369c423ccbcb540f426c64
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-21 16:21:40 -0300

    feat: add home router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/",
+    element: <Home />,
   },
 ]);
 
