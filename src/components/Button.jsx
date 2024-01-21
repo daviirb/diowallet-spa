66e5d02e001ccb7fb7e31fc2defa18fbda3877f5
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-21 16:22:57 -0300

    feat: improvements to the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
+import { BiPlusCircle, BiMinusCircle } from "react-icons/bi";
+
 // eslint-disable-next-line react/prop-types
-export default function Button({ type, text }) {
+export default function Button({ type, text, icon }) {
+  if (!icon) icon = "";
+  let IconComponent;
+
+  if (icon === "plus") IconComponent = BiPlusCircle;
+  if (icon === "minus") IconComponent = BiMinusCircle;
+
   return (
     <button
       type={type}
-      className="px-4 py-2 rounded w-full font-bold text-white bg-gradient-to-r from-sky-500 to-indigo-500"
+      className="px-4 py-2 rounded w-full flex items-center justify-center gap-2 font-bold text-white bg-gradient-to-r from-sky-500 to-indigo-500"
     >
-      {text}
+      {IconComponent && <IconComponent />} {text}
     </button>
   );
 }
