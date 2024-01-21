5665ae8ddbea04b362ee85873bd903b0a0cba6a1
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-21 13:35:09 -0300

    feat: add Zod and Use-form

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,14 +2,43 @@ import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/logo.png";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ErrorInput from "../components/ErrorInput";
+import { signinSchema } from "../schemas/SigninSchema";
 
 export default function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(signinSchema) });
+
+  function handleSubmitForm(data) {
+    console.log(data);
+  }
+
   return (
     <div className="flex flex-col items-center justify-around rounded p-8 w-[32rem] h-[32rem] bg-zinc-900 ">
       <img src={logo} alt="" className="w-44" />
-      <form className="flex flex-col justify-center w-full gap-2">
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <form
+        onSubmit={handleSubmit(handleSubmitForm)}
+        className="flex flex-col justify-center w-full gap-2"
+      >
+        <Input
+          type="email"
+          placeholder="Email"
+          register={register}
+          name="email"
+        />
+        {errors.email && <ErrorInput text={errors.email.message} />}
+        <Input
+          type="password"
+          placeholder="Password"
+          register={register}
+          name="password"
+        />
+        {errors.password && <ErrorInput text={errors.password.message} />}
         <Button type="submit" text="SIGNIN" />
       </form>
       <p className="text-white">
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,64 @@
-import Button from "../components/Button";
-import Input from "../components/Input";
-import logo from "../assets/logo.png";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { BiArrowBack } from "react-icons/bi";
 import { Link, Outlet } from "react-router-dom";
+import logo from "../assets/logo.png";
+import Button from "../components/Button";
+import ErrorInput from "../components/ErrorInput";
+import Input from "../components/Input";
+import { signupSchema } from "../schemas/SignupSchema";
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(signupSchema) });
+
+  function handleSubmitForm(data) {
+    console.log(data);
+  }
+
   return (
     <div className="flex flex-col items-center justify-around rounded p-8 w-[32rem] h-[32rem] relative bg-zinc-900 ">
       <img src={logo} alt="" className="w-44" />
       <Link to="/signin">
         <BiArrowBack className="absolute top-3 left-3 text-white text-xl hover:text-sky-600" />
       </Link>
-      <form className="flex flex-col justify-center w-full gap-2">
-        <Input type="text" placeholder="Nome Completo" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Senha" />
-        <Input type="password" placeholder="Confirme a Senha" />
+      <form
+        onSubmit={handleSubmit(handleSubmitForm)}
+        className="flex flex-col justify-center w-full gap-2"
+      >
+        <Input
+          type="text"
+          placeholder="Nome Completo"
+          register={register}
+          name="name"
+        />
+        {errors.name && <ErrorInput text={errors.name.message} />}
+        <Input
+          type="email"
+          placeholder="Email"
+          register={register}
+          name="email"
+        />
+        {errors.email && <ErrorInput text={errors.email.message} />}
+        <Input
+          type="password"
+          placeholder="Senha"
+          register={register}
+          name="password"
+        />
+        {errors.password && <ErrorInput text={errors.password.message} />}
+        <Input
+          type="password"
+          placeholder="Confirme a Senha"
+          register={register}
+          name="confirmPassword"
+        />
+        {errors.confirmPassword && (
+          <ErrorInput text={errors.confirmPassword.message} />
+        )}
         <Button type="submit" text="SIGNUP" />
       </form>
       <Outlet />
